48d5e2a10e98c94b6e2a2d592fbb26d57b6c20d6
shaiTheKimhi <shaikimhi221199@gmail.com>
2021-07-15 02:19:22 +0300

    Datasets and tests

---
+++ .ipynb_checkpoints/experiments-checkpoint.py
@@ -0,0 +1,244 @@
+import os
+import sys
+import json
+import torch
+import random
+import argparse
+import itertools
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10, MNIST
+
+from utils.train_results import FitResult
+
+import LinearNetwork as LN, training
+
+DATA_DIR = os.path.expanduser("~/.pytorch-datasets")
+
+MODEL_TYPES = dict(
+    fc=LN.LinearClassifier, resnet=torchvision.models.resnet18#, ycn=cnn.YourCodeNet
+)
+
+
+def run_experiment(
+    run_name,
+    out_dir="./results",
+    seed=None,
+    device=None,
+    # Training params
+    bs_train=128,
+    bs_test=None,
+    batches=100,
+    epochs=100,
+    early_stopping=3,
+    checkpoints=None,
+    lr=1e-3,
+    reg=1e-3,
+    # Model params
+    filters_per_layer=[64],
+    layers_per_block=2,
+    pool_every=2,
+    hidden_dims=[1024],
+    model_type="fc",
+    dataset="mnist",
+    # You can add extra configuration for your experiments here
+    **kw,
+):
+    """
+    Executes a single run of a Part3 experiment with a single configuration.
+
+    These parameters are populated by the CLI parser below.
+    See the help string of each parameter for it's meaning.
+    """
+    if not seed:
+        seed = random.randint(0, 2 ** 31)
+    torch.manual_seed(seed)
+    if not bs_test:
+        bs_test = max([bs_train // 4, 1])
+    cfg = locals()
+
+    tf = torchvision.transforms.ToTensor()
+    #change datasets
+    #ds_train = CIFAR10(root=DATA_DIR, download=True, train=True, transform=tf)
+    #ds_test = CIFAR10(root=DATA_DIR, download=True, train=False, transform=tf)
+    ds_train = MNIST(root=DATA_DIR, download=True, train=True, transform=tf)
+    ds_test = MNIST(root=DATA_DIR, download=True, train=False, transform=tf)
+
+    if not device:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Select model class
+    if model_type not in MODEL_TYPES:
+        raise ValueError(f"Unknown model type: {model_type}")
+    model_cls = MODEL_TYPES[model_type]
+
+    
+    ######################
+    num_classes = 10
+    loss_fn = torch.nn.CrossEntropyLoss().to(device)
+
+    channels = [f for f in filters_per_layer for i in range(layers_per_block)]
+    my_args = dict(conv_params=dict(kernel_size=3, stride=1, padding=1), pooling_params=dict(kernel_size=2, stride=2, padding=1))
+    if model_type == "fc":
+        my_model = model_cls(in_size=784, out_classes=num_classes, hidden_dims=hidden_dims).to(device)
+    else:
+        my_model = model_cls(in_size=ds_train[0][0].shape, out_classes=num_classes, channels=channels, pool_every=pool_every, hidden_dims=hidden_dims, **my_args).to(device)
+
+    opt = torch.optim.Adam(params=my_model.parameters(), lr=lr, weight_decay=reg)
+    train_ing = training.TorchTrainer(my_model, loss_fn, opt, device)
+    dl_train = DataLoader(ds_train, bs_train, shuffle=True)
+    dl_test = DataLoader(ds_test, bs_test, shuffle=False)
+    fit_res = train_ing.fit(dl_train, dl_test, num_epochs=epochs, checkpoints=checkpoints, early_stopping=early_stopping, **kw)
+
+
+    # ========================
+
+    save_experiment(run_name, out_dir, cfg, fit_res)
+
+
+def save_experiment(run_name, out_dir, cfg, fit_res):
+    output = dict(config=cfg, results=fit_res._asdict())
+
+    cfg_LK = (
+        f'L{cfg["layers_per_block"]}_K'
+        f'{"-".join(map(str, cfg["filters_per_layer"]))}'
+    )
+    output_filename = f"{os.path.join(out_dir, run_name)}_{cfg_LK}.json"
+    os.makedirs(out_dir, exist_ok=True)
+    with open(output_filename, "w") as f:
+        json.dump(output, f, indent=2)
+
+    print(f"*** Output file {output_filename} written")
+
+
+def load_experiment(filename):
+    with open(filename, "r") as f:
+        output = json.load(f)
+
+    config = output["config"]
+    fit_res = FitResult(**output["results"])
+
+    return config, fit_res
+
+
+def parse_cli():
+    p = argparse.ArgumentParser(description="CS236781 HW2 Experiments")
+    sp = p.add_subparsers(help="Sub-commands")
+
+    # Experiment config
+    sp_exp = sp.add_parser(
+        "run-exp", help="Run experiment with a single " "configuration"
+    )
+    sp_exp.set_defaults(subcmd_fn=run_experiment)
+    sp_exp.add_argument(
+        "--run-name", "-n", type=str, help="Name of run and output file", required=True
+    )
+    sp_exp.add_argument(
+        "--out-dir",
+        "-o",
+        type=str,
+        help="Output folder",
+        default="./results",
+        required=False,
+    )
+    sp_exp.add_argument(
+        "--seed", "-s", type=int, help="Random seed", default=None, required=False
+    )
+    sp_exp.add_argument(
+        "--device",
+        "-d",
+        type=str,
+        help="Device (default is autodetect)",
+        default=None,
+        required=False,
+    )
+
+    # # Training
+    sp_exp.add_argument(
+        "--bs-train",
+        type=int,
+        help="Train batch size",
+        default=128,
+        metavar="BATCH_SIZE",
+    )
+    sp_exp.add_argument(
+        "--bs-test", type=int, help="Test batch size", metavar="BATCH_SIZE"
+    )
+    sp_exp.add_argument(
+        "--batches", type=int, help="Number of batches per epoch", default=100
+    )
+    sp_exp.add_argument(
+        "--epochs", type=int, help="Maximal number of epochs", default=100
+    )
+    sp_exp.add_argument(
+        "--early-stopping",
+        type=int,
+        help="Stop after this many epochs without " "improvement",
+        default=3,
+    )
+    sp_exp.add_argument(
+        "--checkpoints",
+        type=int,
+        help="Save model checkpoints to this file when test " "accuracy improves",
+        default=None,
+    )
+    sp_exp.add_argument("--lr", type=float, help="Learning rate", default=1e-3)
+    sp_exp.add_argument("--reg", type=float, help="L2 regularization", default=1e-3)
+
+    # # Model
+    sp_exp.add_argument(
+        "--filters-per-layer",
+        "-K",
+        type=int,
+        nargs="+",
+        help="Number of filters per conv layer in a block",
+        metavar="K",
+        required=True,
+    )
+    sp_exp.add_argument(
+        "--layers-per-block",
+        "-L",
+        type=int,
+        metavar="L",
+        help="Number of layers in each block",
+        required=True,
+    )
+    sp_exp.add_argument(
+        "--pool-every",
+        "-P",
+        type=int,
+        metavar="P",
+        help="Pool after this number of conv layers",
+        required=True,
+    )
+    sp_exp.add_argument(
+        "--hidden-dims",
+        "-H",
+        type=int,
+        nargs="+",
+        help="Output size of hidden linear layers",
+        metavar="H",
+        required=True,
+    )
+    sp_exp.add_argument(
+        "--model-type",
+        "-M",
+        choices=MODEL_TYPES.keys(),
+        default="cnn",
+        help="Which model instance to create",
+    )
+
+    parsed = p.parse_args()
+
+    if "subcmd_fn" not in parsed:
+        p.print_help()
+        sys.exit()
+    return parsed
+
+
+if __name__ == "__main__":
+    parsed_args = parse_cli()
+    subcmd_fn = parsed_args.subcmd_fn
+    del parsed_args.subcmd_fn
+    print(f"*** Starting {subcmd_fn.__name__} with config:\n{parsed_args}")
+    subcmd_fn(**vars(parsed_args))
