a5d8ee08c238f220432393869d8c2b287aadf1e9
shaiTheKimhi <shaikimhi221199@gmail.com>
2021-08-03 00:25:32 +0300

    sklearn random projection, could try complete

--- RandomProjection.py
+++ RandomProjection.py
@@ -3,8 +3,14 @@ import torch.nn as nn
 from sklearn import random_projection
 import torchviz
 
-# RP layer could be a Linear Layer, with unlearned values that are sampled from N(0,1/n)
-# for convenience, we would prefer defining a new layer that calculates gradient only by X and not by W (this might be implemented on a linear layer, check later)
+#RP matrix: R of shape (d,n), input vector X of shape (B,d) output: XR of shape (B,n)
+#R is output of the following functions, given a matrix we will define a Linear projection layer
+
+
+"""
+RP layer could be a Linear Layer, with unlearned values that are sampled from N(0,1/n)
+for convenience, we would prefer defining a new layer that calculates gradient only by X and not by W (this might be implemented on a linear layer, check later) 
+"""
 
 #RP layers will be returned through designated functions, will be returned as matrix tensor
 
@@ -13,7 +19,7 @@ def Gaussian(original_d:int, projected_n:int):
     n = projected_n
     d = original_d
     transformer = random_projection.GaussianRandomProjection(n)
-    return torch.tensor(transformer.fit_transform(torch.eye(d,d))).T
+    return torch.tensor(transformer.fit_transform(torch.eye(d,d)))
     #return torch.normal(mean=torch.zeros(n,d), std=torch.ones(n,d)/(n*d))
 
 #TODO: add Li(sparse gaussian) and count sketch random projectors
