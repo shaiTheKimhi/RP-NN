79ef00f044ecfed5d95e6072b791576a4426ab67
shaiTheKimhi <shaikimhi221199@gmail.com>
2021-09-03 02:58:41 +0300

    added Count Sketch

--- RandomProjection.py
+++ RandomProjection.py
@@ -1,8 +1,10 @@
+import math
 import torch
 import torch.nn as nn
 from sklearn import random_projection
 import torchviz
 from scipy.linalg import hadamard
+import torch.nn.functional as F
 
 #RP matrix: R of shape (d,n), input vector X of shape (B,d) output: XR of shape (B,n)
 #R is output of the following functions, given a matrix we will define a Linear projection layer
@@ -31,7 +33,7 @@ def Achlioptas(original_d:int, projected_k:int):
     k = projected_k
     d = original_d
     probs = torch.tensor([1/6,2/3,1/6])
-    achl = torch.distribtutions.Categorical(torch.ones(k,d,3)*probs)
+    achl = torch.distributions.Categorical(torch.ones(k,d,3)*probs)
     return (achl.sample()-1) * (3/k)**0.5
 
 
@@ -42,23 +44,40 @@ def Li(original_d:int, projected_k:int):
     d = original_d
     s = d**0.5 #largest bound for s is d/logd, recommended sqrt(d)
     probs = torch.tensor([1/(2*s),1-1/s,1/(2*s)])
-    dist = torch.distribtutions.Categorical(torch.ones(k,d,3)*probs)
+    dist = torch.distributions.Categorical(torch.ones(k,d,3)*probs)
     return (dist.sample()-1) * (s/k)**0.5
 
-#SRHT (Li with densifier)
-def SRHT(original_d:int, projected_k:int): #for SRHT d must be a power of 2, if not we can padd the number of features to be a power of 2
+#SRHT (Li with densifier) 
+def SRHT(original_d:int, projected_k:int, dataset_n:int = 6e4): #for SRHT d must be a power of 2, if not we can padd the number of features to be a power of 2
+    #TODO: add padding to vector for k to be power of 2
     k = projected_k
     d = original_d
+    n = dataset_n
+    q = (math.log(n, 10)**2)/d # q is O(log^2(n)/d) we chose 10 as base of log for convenience
+    q = 1 if q > 1 else q if q >= 0 else 0 # q is probability value
 
-    B = torch.distribtutions.Bernoulli(torch.ones(k,d)*0.5)
-    D = B.sample()*2 - 1
-    H = ((1/d)**0.5)*hadamard(d)
-    return D@H
-    #TODO: return P = DHS
+    B = torch.distributions.Bernoulli(torch.ones(d)*0.5)
+    D = torch.diag(B.sample()*2 - 1) # D is d X d
+    H = ((1/d)**0.5)*hadamard(d) # H is d X d, S is d X k
+    S = torch.normal(mean=torch.zeros(k,d), std=torch.ones(k,d)/d**0.5) * torch.distributions.Bernoulli(torch.ones(k,d)*0.5).sample()
+    return S@D@H/k**0.5
 
 
 
-#TODO: add count-sketch random dimensionality reduction using sklearn
+#count-sketch 
+def CountSketch(original_d: int, projected_k:int):
+    k = projected_k
+    d = original_d
+    B = torch.distributions.Bernoulli(torch.ones(d)*0.5)
+    D = torch.diag(B.sample()*2 - 1) # D is d X d
+    probs = torch.ones(k)/k
+    m = torch.distributions.Categorical(torch.ones(d,k)*torch.tensor(probs))
+    A = F.one_hot(m.sample()+1, num_classes=k+1)
+    C = A.T[torch.arange(A.size(1))!=0] # C is d X k
+    C = torch.tensor(C, dtype=torch.float32)
+    D = torch.tensor(D, dtype=torch.float32)
+    return (C @ D) #possible: calc C.T @ D could be easier to calculate
+
 
 
 #TODO: add an attempt to create PCA dimensional reduction as the 'optimal' solution for comparsion
