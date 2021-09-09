5cee1152a80cb3379ab23201c7ffbcd39252997d
shaiTheKimhi <shaikimhi221199@gmail.com>
2021-09-09 17:54:10 +0300

    working with SRHT

--- LinearNetwork.py
+++ LinearNetwork.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import itertools as it
 from typing import Sequence
+import torch.nn.functional as F
 
 import RandomProjection as RP
 ACTIVATIONS = {"relu": nn.ReLU, "lrelu": nn.LeakyReLU, "sigmoid": nn.Sigmoid, "tanh": nn.Tanh}
@@ -12,16 +13,18 @@ class LinearClassifier(nn.Module):
     """
     Linear Fully Connected Neural Network model
     """
-    def __init__(self, in_size, out_classes:int, activation_type: str = "relu", activation_params: dict = None, hidden_dims : list = [196, 49], rp : int = -1, rp_type:str = 'Gaussian'):
+    def __init__(self, in_size, out_classes:int, activation_type: str = "relu", activation_params: dict = None, hidden_dims : list = [196, 49], rp : int = -1, rp_type:str = 'Gaussian', padding=0):
         super().__init__()
+        self.padding = padding
         self.in_size = in_size
         self.out_classes = out_classes
         self.activation_type = ACTIVATIONS[activation_type]
         self.activation_params = activation_params
-        
+        in_size += padding
         layers = []
         if rp >= 1:
             W = PROJECTIONS[rp_type](in_size, rp)
+            W = torch.tensor(W, dtype=torch.float32)
             rplayer = nn.Linear(in_size, rp)
             rplayer.weight = nn.Parameter(W, requires_grad=False)
             layers.append(rplayer)
@@ -37,9 +40,9 @@ class LinearClassifier(nn.Module):
         seq = nn.Sequential(*layers)
         self.classifier = seq
 
-    def forward(self, x):
+    def forward(self, x): #x of shape (B,1,N,M)
         x = x.reshape(x.size(0), -1) #flatten the input vector (I think flattens to a matrix, might change to only -1 to flatten to a vector)
-        size = x.size(0)*x.size(1)
+        x = F.pad(x, (1,self.padding-1))##padding input 
         #x = x.reshape(1,size)
         out = self.classifier(x) #FC Classifier (with RP at start)
         return out
